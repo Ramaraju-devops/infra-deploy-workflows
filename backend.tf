86335dc489b0eeae09515253e7adbd2d2f55645a
Ramaraju <103272173+Ramaraju-devops@users.noreply.github.com>
2023-07-29 12:48:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "github-tfstates"
+    resource_group_name  = "github-tfstates-rg"
     storage_account_name = "stracctgithubterr01"
     container_name       = "tfstatecontainer"
     key                  = "terragithubstate.tfstate"
