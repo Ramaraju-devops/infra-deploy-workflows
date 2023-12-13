4f6ba7daec39edd6b91e62eac3a7d5d6c8987c98
rama <rraj95155@outlook.com>
2023-12-13 10:52:16 +0530

    test ci workflow

--- terraform-script/main.tf
+++ terraform-script/main.tf
@@ -7,3 +7,13 @@ resource "azurerm_resource_group" "demogithubrg2" {
   }
 }
 
+resource "azurerm_resource_group" "demogithubrg3" {
+  name     = "demogithubwfrg002"
+  location = var.location
+  tags = {
+    "owner"   = "rama"
+    "purpose" = "demo"
+  }
+}
+
+
