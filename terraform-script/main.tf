b9c2400093e53781b4d7385ac671c1290efb4f92
ramu <rraj95155@gmail.com>
2025-06-18 22:07:19 +0530

    github actions test 2

--- terraform-script/main.tf
+++ terraform-script/main.tf
@@ -9,5 +9,16 @@ resource "azurerm_resource_group" "demorg01" {
   }
 }
 
+resource "azurerm_resource_group" "demorg02" {
+  name     = "demaghactionapr25-rg2"
+  location = var.location
+  tags = {
+    "owner"   = "rama"
+    "purpose" = "demo"
+    "environment" = "testing"
+  }
+}
+
+
 
 
